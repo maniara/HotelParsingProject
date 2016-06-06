3d2c614d2ded97d129544543d5f89cb70b8c6042
deog-yungo <deog-yungo@192.168.0.4>
2016-06-06 21:22:55 +0900

    160606

--- HotelParse/src/HotelParse/ExpediaParser.java
+++ HotelParse/src/HotelParse/ExpediaParser.java
@@ -7,10 +7,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
 import net.htmlparser.jericho.Element;
 import net.htmlparser.jericho.HTMLElementName;
 import net.htmlparser.jericho.Source;
@@ -26,7 +22,7 @@ public class ExpediaParser {
 	
 	
 	public static ArrayList<RoomPrice> getRooms(String hotelName, String checkInDate, String checkOutDate)
-			throws MalformedURLException, IOException, ParseException {
+			throws MalformedURLException, IOException {
 		//System.out.println("Get room information : "+checkInDate);
 		ExpediaParser.hotelName = hotelName;
 		ExpediaParser.checkInDate = checkInDate;
@@ -35,34 +31,49 @@ public class ExpediaParser {
 		//String url = "https://www.expedia.co.kr/"+hotelName+".h6084370.Hotel-Information?chkin="+checkInDate+"&chkout="+checkOutDate+"&rm1=a2#rooms-and-rates";
 		//System.out.println(url);
 		String url = "https://www.expedia.co.kr/Seoul-Hotels-Nine-Tree-Hotel.h6084370.Hotel-Information?chkin=2016.06.13&chkout=2016.06.14&rm1=a2&hwrqCacheKey=b8bbe2f0-0d2f-41ec-9db8-ca025f4530f0HWRQ1464857938255&c=30966cb8-c3d9-4276-bc5c-5a3311cfe3d7&";
-		
+		String htmlString = SeleniumRequester.getResponse(url);
 		
 		//if Null data comes flag became false
 		while (successFlag == false) {
 			
+			Source source = new Source(htmlString);
+			/*for(Element e :source.getAllElementsByClass("room room-above-fold")){
+				System.out.println(e.getFirstElementByClass("room-basic-info").toString());
+			successFlag = true;
+			}*/
 			
-			Source source = new Source(new URL(url));
 			
-			for(Element e : source.getAllElements("script"))
+			
+			//No room on the dates
+			if(source.getFirstElementByClass("room-basic-info") == null)
+				return roomPriceList;
+			
+			//No type on the dates
+			if(source.getFirstElementByClass("room-basic-info").getAllElements("h3") == null)
+				continue;
+			
+			//No type on the dates
+			if(source.getAllElementsByClass("room room-above-fold first-room-featured").size()==0)
+				continue;
+			
+			/*for (Element e1 : source.getAllElementsByClass("room room-above-fold first-room-featured"))  //firstRoomType
 			{
-				if(e.toString().contains("var utag_data = ")){
-					getJson(e.getContent().toString().replace("var utag_data = ", "").replace(";", ""));
-				}
+				//System.out.println("first");
+				getRoomTypeInformation(e1);
+			}*/
+
+			//Loop on each type
+			for (Element e1 : source.getAllElementsByClass("room room-above-fold"))  
+			{
+				//System.out.println("second");
+				getRoomTypeInformation(e1);
 			}
-		
 		}
 		successFlag = false;
 		return roomPriceList;
 	}
 
-	private static void getJson(String jsonString) throws ParseException {
-		
-		JSONParser jsonParser = new JSONParser();
-		JSONObject object = (JSONObject) jsonParser.parse(jsonString);
-		System.out.println(object);
-	}
-
-/*	private static void getRoomTypeInformation(Element e1) {
+	private static void getRoomTypeInformation(Element e1) {
 		RoomPrice rp = new RoomPrice("Expedia",ExpediaParser.hotelName,ExpediaParser.checkInDate);
 		int minPrice = 10000000;
 		
@@ -133,5 +144,5 @@ public class ExpediaParser {
 		if(optionElement.getAllElementsByClass("free-cancellation-short free-text").size() != 0)
 			retVal = true;
 		return retVal;
-	}*/
+	}
 }
